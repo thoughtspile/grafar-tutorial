93da6a0ee03d61f8037b72e68d66f9e26213c0f4
Vladimir Klepov <v.klepov@gmail.com>
2016-10-31 11:30:44 +0300

    html generation

---
+++ build-html.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const hbsDir = './hbs/';
+const outDir = './html/';
+
+const template = (locals) => `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>${ locals.name || locals.scriptName }</title>
+      <style>
+        .panel {
+          width: 400px;
+          height: 400px;
+          display: inline-block;
+        }
+      </style>
+    </head>
+    <body>
+      ${ locals.body }
+      <script src='../../node_modules/grafar/build/grafar.js'></script>
+      <script src='../../build/${ locals.scriptPath }'></script>
+    </body>
+  </html>
+`;
+
+const pathList = [];
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir);
+}
+fs.readdirSync(hbsDir).forEach(dir => {
+  if (!fs.existsSync(path.join(outDir, dir))) {
+    fs.mkdirSync(path.join(outDir, dir));
+  }
+  fs.readdirSync(path.join(hbsDir, dir)).forEach(file => {
+    const body = fs.readFileSync(path.join(hbsDir, dir, file));
+    const rawName = file.replace(/\.[^.]*$/, '');
+    const html = template({
+      scriptName: file,
+      scriptPath: path.join(dir, rawName + '.js'),
+      body
+    });
+    pathList.push({ name: rawName, path: path.join(outDir, dir, file) });
+    fs.writeFileSync(path.join(outDir, dir, file), html);
+  });
+});
+
+fs.writeFileSync('./index.html', template({
+  body: pathList.map(item => `<a href='${ item.path }'>${ item.name }</a>`).join('<br>')
+}));
--- html/03.dynamic/03.sync.js
+++ html/03.dynamic/03.sync.js
@@ -1,1 +1,21 @@
-// еще никак
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>03.sync.js</title>
+      <style>
+        .panel {
+          width: 400px;
+          height: 400px;
+          display: inline-block;
+        }
+      </style>
+    </head>
+    <body>
+      // еще никак
+
+      <script src='../../node_modules/grafar/build/grafar.js'></script>
+      <script src='../../build/03.dynamic/03.sync.js'></script>
+    </body>
+  </html>
